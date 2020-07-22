7cd8f0b31686b531f487e1a597eef78544d1d5b2
Matthew Steele <matthewsteeleonline@gmail.com>
2020-07-22 19:43:17 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,8 +1,8 @@
 chance_of_rain = 0.2
-puts "Let's go outside!"
-if chance_of_rain > 0.5
+if chance_of_rain <= 0.25
+  puts "Pack a sun shelter!"
+elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
   puts "Pack an umbrella!"
 else
-  puts "Enjoy the fine day!"
+  puts "Stay home and read Hegel."
 end
-puts "Oh, and always wear sunscreen!"
